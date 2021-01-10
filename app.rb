61ac8217f69af7d5a6eef78a19557a20ae9759af
shmuly11 <shmuly11@gmail.com>
2021-01-10 14:07:55 -0500

    commit tree 77106475541d526b4921298fb118a425906d06b1

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+    end
+
+    get '/name' do
+        "My name is bigsysmalls"
+    end
+
+    get '/hometown' do
+        "My hometown is compton"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is my name is"
+    end
 end
